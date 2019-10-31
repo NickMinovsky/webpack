5e9b62f95b4c3e42849dd53d7f862ca0129061e2
Nick MInovsky <nickminovsky@gmail.com>
2019-10-31 15:56:41 +0200

    added eslint/babel-lint

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -1,3 +1,4 @@
 import React from 'react';
 
-export default () => <span className="warning">Take it easy!</span>;
+const Warning = () => <span className="warning">Take it easy!</span>;
+export default Warning;
